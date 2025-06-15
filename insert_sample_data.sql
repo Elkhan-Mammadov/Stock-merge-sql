8ce5c535b2763c46d493771f438c564ada4416f3
Elkhan-Mammadov <elkhanmammadov22@gmail.com>
2025-06-15 09:51:24 +0400

    Add files via upload

---
+++ insert_sample_data.sql
@@ -0,0 +1,7 @@
+-- stock table insert
+INSERT INTO stock VALUES (1, 'Coffee Beans', 100, TO_DATE('2025-06-10', 'YYYY-MM-DD'));
+INSERT INTO stock VALUES (2, 'Tea Leaves', 50, TO_DATE('2025-06-12', 'YYYY-MM-DD'));
+
+-- srock_incoming insert 
+INSERT INTO stock_incoming VALUES (2, 'Tea Leaves Premium', 20, TO_DATE('2025-06-15', 'YYYY-MM-DD'));
+INSERT INTO stock_incoming VALUES (3, 'Sugar', 40, TO_DATE('2025-06-15', 'YYYY-MM-DD'));
